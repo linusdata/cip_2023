cbd4381144b61ba0c34ff82c70475a935be5b8ab
Nicolas Good <n.good@hotmail.com>
2023-05-15 17:42:20 +0200

    boxoffice skripte löschen

--- cip_library.py
+++ cip_library.py
@@ -170,24 +170,3 @@ def preprocess_text_for_sentiment(text):
     #vorverarbeiteten text zurück geben
     return processed_text
 
-
-def delete_random_data(df, deletion_rate=0.1):
-
-    """
-    Eine einfache Möglichkei einen Datensatz zu importieren und dann nach dem Zufallsprinzip einen bestimmten Prozentsatz (standardmäßig 10 %) der Daten zu löschen.
-    :param df: dataframe auf welches die Funktion angewendet werden soll.
-    :param deletion_rate: Prozentsatz der Daten, welche gelöscht werden sollen.
-    :return: gibt "dirty" data zurück
-    """
-
-    dirty_df = df.copy()
-    for column in dirty_df.columns:
-        if dirty_df[column].dtype == "object":
-            for idx, _ in dirty_df.iterrows():
-                if random.random() < deletion_rate:
-                    dirty_df.at[idx, column] = None
-        else:
-            for idx, _ in dirty_df.iterrows():
-                if random.random() < deletion_rate:
-                    dirty_df.at[idx, column] = float('nan')
-    return dirty_df
